3e608e1005da01d0d17b43c1c1ea2c5fd0006d97
Bajithan Sivathasan <247406187+Bajithan@users.noreply.github.com>
2026-06-09 09:03:45 +0530

    feat:websocket error solved

--- backend/src/websocket/socket.js
+++ backend/src/websocket/socket.js
@@ -1,8 +1,4 @@
-// MEMBER 4 — This file is yours to build
-// This is a placeholder so server.js does not break on startup
-
 const initSocket = (httpServer) => {
-  // Member 4 will implement Socket.io here
   console.log('WebSocket placeholder ready for Member 4');
 };
 
