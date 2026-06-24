6f0d662ea23a7a231701b7078204306cb82db889
Bajithan Sivathasan <247406187+Bajithan@users.noreply.github.com>
2026-06-24 22:43:21 +0530

    feat:web socket issue resolved

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,18 @@
 const { createClient } = require('@supabase/supabase-js');
+const ws = require('ws');
 const { SUPABASE_URL, SUPABASE_ANON_KEY } = require('./env');
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error('Missing Supabase credentials in environment variables');
 }
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: false,
+  },
+  realtime: {
+    websocket: ws,
+  },
+});
 
 module.exports = supabase;
